cd4d690c5bd889f91b6d7f233b9ae4ffc59394f8
shahidhabib123 <shahidshehryarkhan@outlook.com>
2023-03-11 22:38:09 +0500

    Javascript Project

---
+++ index.js
@@ -0,0 +1,20 @@
+// /*function functionName(a){
+//      console.log("Office Open")
+//      console.log("Petty Cash Count")
+// return  2+2
+// } */
+// let message  = "Hello World"
+// console.log(message);
+//console.log("Shahid is a good guy");
+//function sum(english:number,math:number){;
+//console.log(`the sum of a+b is $ {a+b} `);
+//}
+//sum (30,76);
+//const sum1 = (a:number,b:number) => {
+//console.log(`The sum of a&b is ${a+b}`);
+//}
+//sum1(10,30);
+var sum1 = function (a, b) {
+    return "The sum of a&b is ".concat(a + b);
+};
+console.log(sum1(10, 30));
---
+++ index.ts
@@ -0,0 +1,28 @@
+// /*function functionName(a){
+//      console.log("Office Open")
+//      console.log("Petty Cash Count")
+// return  2+2
+// } */
+
+// let message  = "Hello World"
+// console.log(message);
+
+//console.log("Shahid is a good guy");
+
+//function sum(english:number,math:number){;
+//console.log(`the sum of a+b is $ {a+b} `);
+//}
+//sum (30,76);
+
+//const sum1 = (a:number,b:number) => {
+//console.log(`The sum of a&b is ${a+b}`);
+
+//}
+//sum1(10,30);
+
+const sum1 = (a:number,b:number) => {
+    return `The sum of a & b is ${a+b}`
+    
+    }
+   console.log( sum1(10,30));
+    
